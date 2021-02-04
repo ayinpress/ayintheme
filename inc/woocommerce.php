deb1d7db61b89eb19d43617e4e1f9f6ae948b238
Jeff Ong <jonger4@gmail.com>
2021-02-03 21:50:03 -0500

    Shop and single product page revisions.

--- inc/woocommerce.php
+++ inc/woocommerce.php
@@ -217,4 +217,7 @@ remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_output_p
 remove_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_price', 10 );
 remove_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart', 10 );
 add_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_single_excerpt', 10 );
+add_action( 'woocommerce_after_single_product_summary', 'woocommerce_product_description_tab', 10 );
 remove_action( 'woocommerce_after_shop_loop_item_title', 'woocommerce_template_loop_price', 10 );
+remove_action( 'woocommerce_before_shop_loop', 'woocommerce_result_count', 20 );
+remove_action( 'woocommerce_before_shop_loop', 'woocommerce_catalog_ordering', 30 );
