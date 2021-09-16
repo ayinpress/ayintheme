a6e4edbcf0804508e17f8719167834201cdaea02
Chris Blair <chris@interlocksolutions.com>
2021-09-16 13:14:25 -0700

    Fiolio updates finished along with a handful of other minor tweaks

--- template-parts/content/content-single.php
+++ template-parts/content/content-single.php
@@ -41,21 +41,62 @@
 		?>
 	</header>
 
-	<div class="entry-content">
-		<?php
-			// get thumbnail for this page
-			if (has_post_thumbnail(get_queried_object_id())) {
-				$thumb = get_the_post_thumbnail(get_queried_object_id());
-			} else {
-				$thumb = '';
+	<div class="entry-content"><?php
+		$all_categories = get_categories( array(
+			'parent' => '0' //get top level categories only
+		));
+
+		// look for the category id for the "folios" category only
+		$folioCatId = 0;
+		$child_cats = null;
+		foreach( $all_categories as $single_category ){
+			if ($single_category->slug == 'folios') {
+				$folioCatId = $single_category->cat_ID;
 			}
-			if (!empty ($thumb) && (in_category('82'))) {
-				echo '<div class="FolioFeaturedImg">';
-				echo $thumb;
-				echo '</div>';
-			};
-		?>
-		<?php
+		}
+
+		// get children of "folios" category only
+		if ($folioCatId > 0) {
+			$child_cats = get_categories( array(
+				'child_of' => $folioCatId
+				//category_display_order
+			));
+		}
+
+		// get list of categories this post is in
+		$objectArray = [];
+		$objectArray[] = get_queried_object_id();
+		$parent_cats = get_categories(
+			array(
+				'object_ids' => $objectArray,
+			)
+		);
+
+		// loop through both arrays to see if there is a match
+		$isFolioPost = false;
+		if ($child_cats && $parent_cats) {
+			foreach ($child_cats as $child) {
+				foreach ($parent_cats as $cat) {
+					if ($cat->term_id == $child->term_id) {
+						$isFolioPost = true;
+						break;
+					}
+				}
+				if ($isFolioPost) break;
+			}
+		}
+
+		// get thumbnail for posts only in child of folios
+		if ($isFolioPost && (has_post_thumbnail(get_queried_object_id()))) {
+			$thumb = get_the_post_thumbnail(get_queried_object_id());
+		} else {
+			$thumb = '';
+		}
+		if (!empty ($thumb)) {
+			echo '<div class="FolioFeaturedImg">';
+			echo $thumb;
+			echo '</div>';
+		};
 
 		the_content(
 			sprintf(
